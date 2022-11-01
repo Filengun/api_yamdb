bbb4edb2f4576edb6b9e1b1809e21d7a4d9f41a7
art-bagel <mr.bagel@yandex.ru>
2022-11-01 17:39:40 +0300

    Добавил:  1. модель User в приложении users  2. в админку пользователя  3. в settings.py константу AUTH_USER_MODEL для использования кастомного юзера по умолчанию.

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'username', 'first_name', 'last_name', 'email', 'role')
+    list_editable = ('role',)
+    search_fields = ('username',)
+    list_filter = ('username',)
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(User, UserAdmin)
