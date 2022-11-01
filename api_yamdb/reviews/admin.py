8356d98c3c4fcb1105d6cc888aa7860bba693817
Дарья Мейкер <imeyker@gmail.com>
2022-11-01 20:15:50 +0400

    Добавлены модели категории, жанры, произведения+зареганы

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Genre, Category, Title #Comment, Review,
 
-# Register your models here.
+admin.site.register(Genre)
+admin.site.register(Category)
+admin.site.register(Title)
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,77 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """Категории произведений"""
+    name = models.CharField(
+        max_length=250,
+        verbose_name='Название категории'
+    )
+    slug = models.SlugField(
+        unique=True,
+        max_length=50,
+        verbose_name='Слаг категории'
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+
+    def __str__(self) -> str:
+        return self.name
+
+class Genre(models.Model):
+    """Жанры произведений"""
+    name = models.CharField(
+        max_length=250,
+        verbose_name='Название категории'
+    )
+    slug = models.SlugField(
+        unique=True,
+        verbose_name='Слаг жанра'
+    )
+
+    class Meta:
+        verbose_name = 'Жанр'
+
+    def __str__(self) -> str:
+        return self.name
+
+class Title(models.Model):
+    """Произведения (база)"""
+    name = models.CharField(
+        max_length=250, 
+        verbose_name='Название',
+        help_text='Введите название',
+    )
+    #поле year не обязательное, думаю, тут нужна валидация, но позже
+    year = models.IntegerField(
+        null=True,
+        blank=True, 
+        verbose_name='Год',
+        help_text='Введите год релиза',
+    )
+    description = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name='Описание произведения',
+        help_text='Введите описание',
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        verbose_name='Категория',
+        blank=True,
+        null=True,
+        help_text='Введите категорию произведения',
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        blank=True,
+        db_index=True
+    )
+
+    class Meta:
+        verbose_name = 'Произведение'
+    
+    def __str__(self):
+        return self.name
