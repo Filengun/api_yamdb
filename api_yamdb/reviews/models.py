d1c029a7f851aa0a067ea3f43be407e11bf512e2
Олег Маслеников <Oleg.maslenikov.2014@mail.ru>
2022-11-03 02:44:57 +0300

    Добавил классы в admin.py \ Доработал модели в review

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,6 +1,10 @@
+from tabnanny import verbose
 from django.db import models
+from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+
+User = get_user_model()
 
 class Category(models.Model):
     """Категории произведений"""
@@ -87,7 +91,7 @@ class Review(models.Model):
         help_text='Напишите отзыв',
     )
     author = models.ForeignKey(
-        User, #надо импортнуть
+        User,
         on_delete=models.CASCADE,
         related_name="reviews",
         verbose_name="Автор отзыва",
@@ -95,7 +99,7 @@ class Review(models.Model):
     title = models.ForeignKey(
         Title,
         on_delete=models.CASCADE,
-        related_name="titles",
+        related_name="reviews",
         verbose_name='Название',
     )
     score = models.IntegerField(
@@ -108,9 +112,13 @@ class Review(models.Model):
     )
     pub_date = models.DateTimeField(
         auto_now_add=True,
-        verbose_name="Дата публикации отзыва",
-        help_text='Дата публикации отзыва'
+        verbose_name="Дата публикации отзыва"
     )
+
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
 
     def __str__(self):
         return self.text
@@ -123,7 +131,7 @@ class Comment(models.Model):
         help_text='Введите комментарий',
     )
     author = models.ForeignKey(
-        User, #надо импортнуть
+        User,
         on_delete=models.CASCADE,
         related_name="comments",
         verbose_name="Автор комментария",
@@ -140,6 +148,10 @@ class Comment(models.Model):
         help_text='Дата публикации коммента'
     )
 
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментраии'
+        
     def __str__(self):
         return self.text
-
